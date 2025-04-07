86c0798fac4a77fbd433136f6f24ac64caf4bb27
Mohan Yin <mohanyin@gmail.com>
2025-04-07 14:51:05 -0400

    port and clean up game of life

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,231 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+  import { GPU } from "@/lib/services/web-gpu";
+  import simulationShader from "@/lib/shaders/compute/simulation.wgsl?raw";
+  import cellShader from "@/lib/shaders/cell.wgsl?raw";
+
+  let canvas: HTMLCanvasElement;
+
+  const GRID_SIZE = 1000;
+  const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
+  const WORKGROUP_SIZE = 8;
+
+  let step = 0; // Track how many simulation steps have been run
+
+  onMount(async () => {
+    const gpu = new GPU();
+    await gpu.init();
+    gpu.setupGPUCanvasRendering(canvas);
+
+    // Create a uniform buffer that describes the grid.
+    const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+    const uniformBuffer = gpu.createUniformBuffer({
+      data: uniformArray,
+      label: "Grid Uniforms",
+    });
+
+    const vertices = new Float32Array([
+      //   X,    Y,
+      -0.8,
+      -0.8, // Triangle 1 (Blue)
+      0.8,
+      -0.8,
+      0.8,
+      0.8,
+
+      -0.8,
+      -0.8, // Triangle 2 (Red)
+      0.8,
+      0.8,
+      -0.8,
+      0.8,
+    ]);
+
+    const vertexBuffer = gpu.createVertexBuffer({
+      data: vertices,
+      label: "Cell vertices",
+    });
+
+    const vertexBufferLayout: GPUVertexBufferLayout = {
+      arrayStride: 8,
+      attributes: [
+        {
+          format: "float32x2" as GPUVertexFormat,
+          offset: 0,
+          shaderLocation: 0, // Position, see vertex shader
+        },
+      ],
+    };
+
+    const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
+    // Create two storage buffers to hold the cell state.
+    const cellStateStorage = [
+      gpu.createStorageBuffer({
+        data: cellStateArray,
+        label: "Cell State A",
+      }),
+      gpu.createStorageBuffer({
+        data: cellStateArray,
+        label: "Cell State B",
+      }),
+    ];
+
+    for (let i = 0; i < cellStateArray.length; ++i) {
+      cellStateArray[i] = Math.random() > 0.6 ? 1 : 0;
+    }
+    gpu.device!.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
+
+    const simulationShaderModule = gpu.createShaderModule(
+      {
+        code: simulationShader,
+      },
+      {
+        WORKGROUP_SIZE: WORKGROUP_SIZE.toString(),
+      },
+    );
+
+    const cellShaderModule = gpu.createShaderModule({
+      code: cellShader,
+    });
+
+    const bindGroupLayout = gpu.device!.createBindGroupLayout({
+      label: "Cell Bind Group Layout",
+      entries: [
+        {
+          binding: 0,
+          visibility:
+            GPUShaderStage.VERTEX |
+            GPUShaderStage.COMPUTE |
+            GPUShaderStage.FRAGMENT,
+          buffer: {}, // Grid uniform buffer
+        },
+        {
+          binding: 1,
+          visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+          buffer: { type: "read-only-storage" }, // Cell state input buffer
+        },
+        {
+          binding: 2,
+          visibility: GPUShaderStage.COMPUTE,
+          buffer: { type: "storage" }, // Cell state output buffer
+        },
+      ],
+    });
+
+    const pipelineLayout = gpu.device!.createPipelineLayout({
+      label: "Cell Pipeline Layout",
+      bindGroupLayouts: [bindGroupLayout],
+    });
+
+    const simulationPipeline = gpu.device!.createComputePipeline({
+      label: "Simulation pipeline",
+      layout: pipelineLayout,
+      compute: {
+        module: simulationShaderModule,
+        entryPoint: "computeMain",
+      },
+    });
+
+    const cellPipeline = gpu.device!.createRenderPipeline({
+      label: "Cell pipeline",
+      layout: pipelineLayout,
+      vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout],
+      },
+      fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [
+          {
+            format: gpu.format!,
+          },
+        ],
+      },
+    });
+
+    const bindGroups = [
+      gpu.device!.createBindGroup({
+        label: "Cell renderer bind group A",
+        layout: bindGroupLayout,
+        entries: [
+          {
+            binding: 0,
+            resource: { buffer: uniformBuffer },
+          },
+          {
+            binding: 1,
+            resource: { buffer: cellStateStorage[0] },
+          },
+          {
+            binding: 2,
+            resource: { buffer: cellStateStorage[1] },
+          },
+        ],
+      }),
+      gpu.device!.createBindGroup({
+        label: "Cell renderer bind group B",
+        layout: bindGroupLayout,
+        entries: [
+          {
+            binding: 0,
+            resource: { buffer: uniformBuffer },
+          },
+          {
+            binding: 1,
+            resource: { buffer: cellStateStorage[1] },
+          },
+          {
+            binding: 2,
+            resource: { buffer: cellStateStorage[0] },
+          },
+        ],
+      }),
+    ];
+
+    function updateGrid() {
+      const encoder = gpu.device!.createCommandEncoder();
+      const computePass = encoder.beginComputePass();
+
+      computePass.setPipeline(simulationPipeline);
+      computePass.setBindGroup(0, bindGroups[step % 2]);
+      const workgroupCount = Math.ceil(GRID_SIZE / WORKGROUP_SIZE);
+      computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
+      computePass.end();
+
+      step++; // Increment the step count
+
+      // Start a render pass
+      const pass = encoder.beginRenderPass({
+        colorAttachments: [
+          {
+            view: gpu.context!.getCurrentTexture().createView(),
+            loadOp: "clear",
+            clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 },
+            storeOp: "store",
+          },
+        ],
+      });
+
+      // Draw the grid.
+      pass.setPipeline(cellPipeline);
+      pass.setBindGroup(0, bindGroups[step % 2]); // Updated!
+      pass.setVertexBuffer(0, vertexBuffer);
+      pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
+
+      // End the render pass and submit the command buffer
+      pass.end();
+      gpu.device!.queue.submit([encoder.finish()]);
+    }
+
+    setInterval(updateGrid, UPDATE_INTERVAL);
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <canvas id="canvas" bind:this={canvas}></canvas>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/lib/services/web-gpu.ts
@@ -0,0 +1,117 @@
+interface CreateUsageSpecificBufferOptions<T extends ArrayBufferView> {
+  data: T;
+  label: string;
+}
+
+export class GPU {
+  private _device: GPUDevice | null = null;
+  private _context: GPUCanvasContext | null = null;
+  private _format: GPUTextureFormat | null = null;
+
+  async init() {
+    if (this._device) {
+      return;
+    }
+
+    if (!navigator.gpu) {
+      throw new Error("WebGPU not supported on this browser.");
+    }
+
+    const adapter = await navigator.gpu.requestAdapter({
+      powerPreference: "high-performance",
+    });
+    if (!adapter) {
+      throw new Error("No appropriate GPUAdapter found.");
+    }
+
+    this._device = await adapter.requestDevice();
+  }
+
+  get device() {
+    if (!this._device) {
+      throw new Error("GPU device not initialized. Please call init() first.");
+    }
+    return this._device;
+  }
+
+  get context() {
+    return this._context;
+  }
+
+  get format() {
+    return this._format;
+  }
+
+  setupGPUCanvasRendering(canvas: HTMLCanvasElement) {
+    this._context = canvas.getContext("webgpu");
+    if (!this._context) {
+      throw new Error("Failed to create WebGPU context.");
+    }
+    this._format = navigator.gpu.getPreferredCanvasFormat();
+    if (!this.device) {
+      throw new Error("GPU device not initialized. Please call init() first.");
+    }
+    this._context.configure({ device: this.device, format: this._format });
+  }
+
+  createAndCopyBuffer<T extends ArrayBufferView>({
+    data,
+    label,
+    usage,
+  }: {
+    data: T;
+    label: string;
+    usage: number;
+  }) {
+    const buffer = this.device.createBuffer({
+      label,
+      size: data.byteLength,
+      usage,
+    });
+    this.device.queue.writeBuffer(buffer, 0, data);
+    return buffer;
+  }
+
+  createUniformBuffer<T extends ArrayBufferView>(
+    options: CreateUsageSpecificBufferOptions<T>,
+  ) {
+    return this.createAndCopyBuffer({
+      ...options,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+  }
+
+  createStorageBuffer<T extends ArrayBufferView>(
+    options: CreateUsageSpecificBufferOptions<T>,
+  ) {
+    return this.createAndCopyBuffer({
+      ...options,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    });
+  }
+
+  createVertexBuffer<T extends ArrayBufferView>(
+    options: CreateUsageSpecificBufferOptions<T>,
+  ) {
+    return this.createAndCopyBuffer({
+      ...options,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+  }
+
+  createShaderModule(
+    options: GPUShaderModuleDescriptor,
+    interpolate?: Record<string, string>,
+  ) {
+    let code = options.code;
+    if (interpolate) {
+      Object.entries(interpolate).forEach(([key, value]) => {
+        code = code.replaceAll(`{{${key}}}`, value);
+      });
+    }
+    return this.device.createShaderModule({
+      ...options,
+      code,
+    });
+  }
+}
